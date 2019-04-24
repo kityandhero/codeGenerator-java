e54359be35a2f13e7396fcd34db22e621b467290
kityandhero <kityandhero@126.com>
2019-04-24 17:32:21 +0800

    init

---
+++ src/codetools/src/test/java/com/lzt/codetools/CodetoolsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lzt.codetools;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodetoolsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
