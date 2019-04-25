c94699819675c8bcd21d245994d24543253ec584
kityandhero <kityandhero@126.com>
2019-04-25 17:04:08 +0800

    init

---
+++ src/operate/codetools/src/main/java/com/lzt/codetools/CodeToolsApplication.java
@@ -0,0 +1,13 @@
+package com.lzt.codetools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodetoolsApplication.class, args);
+    }
+
+}
