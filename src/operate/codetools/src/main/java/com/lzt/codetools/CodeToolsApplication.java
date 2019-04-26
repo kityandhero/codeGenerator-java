15ca0233b351d303972ad9e39d93e61f1ca1ed01
luzhitao <kityandhero@126.com>
2019-04-26 22:43:47 +0800

    重构项目结构

--- src/operate/codetools/src/main/java/com/lzt/codetools/CodeToolsApplication.java
+++ src/operate/codetools/src/main/java/com/lzt/codetools/CodeToolsApplication.java
@@ -3,11 +3,14 @@ package com.lzt.codetools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author lzt
+ */
 @SpringBootApplication
 public class CodeToolsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CodetoolsApplication.class, args);
+        SpringApplication.run(CodeToolsApplication.class, args);
     }
 
 }
--- src/operate/codetools/src/main/java/com/lzt/codetools/ServletInitializer.java
+++ src/operate/codetools/src/main/java/com/lzt/codetools/ServletInitializer.java
@@ -3,11 +3,14 @@ package com.lzt.codetools;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author lzt
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(CodetoolsApplication.class);
+        return application.sources(CodeToolsApplication.class);
     }
 
 }
