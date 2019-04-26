438ba8cfd59410ba18dc65a895dd43155e069c3b
luzhitao <kityandhero@126.com>
2019-04-26 22:46:13 +0800

    重构项目结构

---
+++ src/operate/codetools/src/main/java/com/lzt/codetools/entrance/controller/EntranceController.java
@@ -0,0 +1,25 @@
+package com.lzt.codetools.entrance.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lzt
+ */
+@RestController
+public class EntranceController {
+    // @RequestMapping("/")
+    // @ResponseBody
+    // public String index(){
+    //
+    //     return "index";
+    // }
+
+    @RequestMapping("/entrance/signIn")
+    @ResponseBody
+    public String signIn(){
+
+        return "success";
+    }
+}
