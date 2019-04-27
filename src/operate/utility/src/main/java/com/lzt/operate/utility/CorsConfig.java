4218c034a605c3ccf30e520c07653b97ede14b55
kityandhero <kityandhero@126.com>
2019-04-27 16:38:33 +0800

    调整代码结构

--- src/operate/utility/src/main/java/com/lzt/operate/utility/CorsConfig.java
+++ src/operate/utility/src/main/java/com/lzt/operate/utility/CorsConfig.java
@@ -1,8 +1,14 @@
 package com.lzt.operate.utility;
 
+import com.google.common.base.Optional;
+import com.google.common.base.Splitter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Set;
+
 
 /**
  * @author lzt
@@ -21,20 +27,40 @@ public class CorsConfig {
     private String accessControlAllowHeaders;
     private String accessControlAllowMethods;
 
-    public String getAllowSites() {
-        return allowSites;
+    private Set<String> mergeToSet(String firstValue, String secordValue) {
+        String v = Optional.of(firstValue).or("");
+        Set<String> set = new HashSet<String>();
+
+
+        Iterable<String> firstValueList = Splitter.on(',').split(v);
+        Iterable<String> secordValueList = Splitter.on(',').split(Optional.of(secordValue).or(""));
+
+        for (String item : firstValueList) {
+            set.add(item);
+        }
+
+        for (String item : secordValueList) {
+            set.add(item);
+        }
+
+        return set;
     }
 
-    public String getNeedAccessControlAllowCredentials() {
-        return allowSites;
+    public Set<String> getAccessControlAllowOriginSet(String existingAllowOrigins) {
+        return mergeToSet(existingAllowOrigins, allowSites);
+    }
+
+    public boolean getNeedAccessControlAllowCredentials() {
+        return needAccessControlAllowCredentials;
     }
 
     public String getAccessControlAllowCredentials() {
         return allowSites;
     }
 
-    public String getAccessControlAllowHeaders() {
-        return allowSites;
+    public Set<String> getAccessControlAllowHeaderSet(String existingAllowHeaders) {
+
+        return mergeToSet(existingAllowHeaders, accessControlAllowHeaders);
     }
 
     public String getAccessControlAllowMethods() {
