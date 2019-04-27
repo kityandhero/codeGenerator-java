c707fb09f429656d661f8c793434fb93231904c7
kityandhero <kityandhero@126.com>
2019-04-27 09:49:27 +0800

    更改为thyemleaf模板引擎

---
+++ src/operate/codetools/src/main/java/com/lzt/codetools/workbench/controller/WorkbenchController.java
@@ -0,0 +1,18 @@
+package com.lzt.codetools.workbench.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author lzt
+ */
+@Controller
+public class WorkbenchController {
+
+    @RequestMapping(value="/hello",method= RequestMethod.GET)
+    public String sayHello(){
+        return "hello";
+    }
+
+}
