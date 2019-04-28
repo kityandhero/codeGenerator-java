190a24ea270be2d7349d2aed63fee14a12dbe2f3
luzhitao <kityandhero@126.com>
2019-04-28 23:26:24 +0800

    增加扩展类

--- src/operate/codetools/src/main/java/com/lzt/operate/codetools/workbench/controller/WorkbenchController.java
+++ src/operate/codetools/src/main/java/com/lzt/operate/codetools/workbench/controller/WorkbenchController.java
@@ -1,5 +1,6 @@
 package com.lzt.operate.codetools.workbench.controller;
 
+import com.lzt.operate.web.controllers.BaseController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @author lzt
  */
 @Controller
-public class WorkbenchController {
+public class WorkbenchController extends BaseController {
 
     @RequestMapping(value = "/main", method = RequestMethod.GET)
     public String sayHello() {
