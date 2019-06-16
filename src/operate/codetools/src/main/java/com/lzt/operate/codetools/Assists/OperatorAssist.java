2f120538d47563fb88770305b1a1096de688b8dd
luzhitao <kityandhero@126.com>
2019-06-17 00:01:25 +0800

    增加获取当前用户的接口

--- src/operate/codetools/src/main/java/com/lzt/operate/codetools/Assists/OperatorAssist.java
+++ src/operate/codetools/src/main/java/com/lzt/operate/codetools/Assists/OperatorAssist.java
@@ -1,15 +1,29 @@
 package com.lzt.operate.codetools.Assists;
 
 import com.lzt.operate.codetools.domain.Operator;
+import com.lzt.operate.codetools.repository.OperatorRepository;
+import com.lzt.operate.secret.DesAssist;
 import com.lzt.operate.utility.RequestAssist;
 import lombok.var;
 
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Operator辅助方法集合
+ *
+ * @author lzt
+ */
 public class OperatorAssist {
-    public static Operator GetCurrent() {
-        var request = RequestAssist.getHttpServletRequest();
 
-        var token = request.getHeader("token");
+    public static Operator getCurrent(OperatorRepository operatorRepository) {
+        HttpServletRequest request = RequestAssist.getHttpServletRequest();
 
-        return null;
+        String token = request.getHeader("token");
+
+        String operatorId = DesAssist.decryptWithCBC(token);
+
+        var optionalResult = operatorRepository.findById(operatorId);
+
+        return optionalResult.orElse(null);
     }
 }
--- src/operate/codetools/src/main/java/com/lzt/operate/codetools/operator/controller/OperatorController.java
+++ src/operate/codetools/src/main/java/com/lzt/operate/codetools/operator/controller/OperatorController.java
@@ -1,30 +1,22 @@
 package com.lzt.operate.codetools.operator.controller;
 
-import com.lzt.operate.codetools.common.GlobalString;
+import com.lzt.operate.codetools.Assists.OperatorAssist;
 import com.lzt.operate.codetools.common.OperateBaseController;
 import com.lzt.operate.codetools.domain.Operator;
 import com.lzt.operate.codetools.repository.OperatorRepository;
-import com.lzt.operate.entity.ParamData;
+import com.lzt.operate.entity.ErrorMessage;
 import com.lzt.operate.entity.ResultDataCore;
 import com.lzt.operate.entity.ResultDataFactory;
 import com.lzt.operate.entity.ResultSingleData;
-import com.lzt.operate.extensions.StringEx;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
-import java.util.Map;
 
 /**
  * @author lzt
@@ -45,47 +37,14 @@ public class OperatorController extends OperateBaseController {
     @ApiOperation(value = "当前用户信息", notes = "当前用户信息", httpMethod = "POST")
     @ApiResponses({@ApiResponse(code = ResultDataFactory.CODE_ACCESS_SUCCESS, message = ResultDataFactory.MESSAGE_ACCESS_SUCCESS, response = ResultSingleData.class)})
     @PostMapping(path = "/getCurrent", consumes = "application/json", produces = "application/json")
-    public ResultDataCore getCurrent(@RequestBody Map<String, String> json) {
-        // 直接将json信息打印出来
-        System.out.println(json);
+    public ResultDataCore getCurrent() {
 
-        ParamData paramJson = new ParamData(json);
+        Operator operator = OperatorAssist.getCurrent(this.operatorRepository);
 
-        // 将获取的json数据封装一层，然后在给返回
-        String name = paramJson.getByKey(GlobalString.LOGIN_USERNAME);
-        String password = paramJson.getByKey(GlobalString.LOGIN_PASSWORD);
-
-        Operator operator = new Operator();
-        operator.setName(name);
-
-        ExampleMatcher matcher = ExampleMatcher.matching()
-                                               .withMatcher("name", ExampleMatcher.GenericPropertyMatchers.ignoreCase())
-                                               .withIgnorePaths("createTime", "password", "friendlyName");
-
-        Example<Operator> example = Example.of(operator, matcher);
-
-        var optionalResult = this.operatorRepository.findFirst(example);
-
-        Operator searchResult = optionalResult.orElse(null);
-
-        if (searchResult == null) {
-            Date now = new Date();
-            operator = new Operator();
-
-            operator.setName(name);
-            operator.setPassword(StringEx.ToMD5(password).toString());
-            operator.setFriendlyName(name);
-            operator.setCreateTime(now);
-
-            Operator operatorSave = this.operatorRepository.save(operator);
-
-            return this.singleData(operatorSave);
+        if (operator != null) {
+            return this.singleData(operator);
         } else {
-            return this.singleData(searchResult);
+            return this.fail(ErrorMessage.noDataError.getCode(), "没有数据");
         }
-
-        // SerializableData data = new SerializableData(json);
-
-        // return this.singleData(data);
     }
 }
