9cbd8c15a1cb6df59faa9b9f5f2f0dc468178b11
kityandhero@126.com <kityandhero@126.com>
2020-04-04 23:03:52 +0800

    调整参数 解决utf8问题

--- tools/start codeToold.bat
+++ tools/start codeToold.bat
@@ -1,1 +1,1 @@
-Java -jar -Xms256m -Xmx400m ../src/operate/code-generator-app/target/code-generator-app-1.0-SNAPSHOT.jar
+Java -jar -Xms256m -Xmx400m -Dfile.encoding=UTF-8  ../src/operate/code-generator-app/target/code-generator-app-1.0-SNAPSHOT.jar
