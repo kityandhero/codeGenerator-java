2237876fefb634f92ec58ec00148ca81bb3e0570
kityandhero <kityandhero@126.com>
2020-06-14 21:37:12 +0800

    update  UI

---
+++ src/operate/code-generator-app/src/main/resources/static/otherInit.js
@@ -0,0 +1,16 @@
+// 此处放置其他初始化代码
+
+window.appInitCustomData = {
+  platformName: '代码生成器',
+  appName: '代码生成器',
+  appDescription: '摒弃虚浮，潜心笃志，坚持以技术为系统建设核心',
+  loginLogo: '/logo.png',
+  shareLogo: '/shareLogo.png',
+  shareLogoName: '代码生成器',
+  leftBarText: '代码生成器',
+  companyName: '代码生成器',
+  copyright: '2020 代码生成器',
+  apiPrefix: {
+    corsTargetProduction: 'http://localhost:9990',
+  },
+};
